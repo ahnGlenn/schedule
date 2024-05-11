1b97f54e541dd2eef641294999c8c0a14910c38d
ansangja@gmail.com <ansangja@gmail.com>
2024-05-11 21:06:40 +0900

    clear git cache

---
+++ src/front/src/redux/store.js
@@ -0,0 +1,8 @@
+// store.js
+import { createStore, applyMiddleware } from 'redux';
+// import thunk from 'redux-thunk';
+// import loginReducer from './login/reducer'
+
+// const store = createStore(loginReducer, applyMiddleware(thunk));
+
+// export default store;
