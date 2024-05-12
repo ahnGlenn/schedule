561d1502c97252acaabc795a1238173f55c8b316
ansangja@gmail.com <ansangja@gmail.com>
2024-05-13 00:06:55 +0900

    dev : create calendar.js and linked with MainPage.js

--- src/front/src/pages/Main/MainPage.js
+++ src/front/src/pages/Main/MainPage.js
@@ -1,14 +1,21 @@
-import LoginPage from "../Login/LoginPage";
+import Calendar from './calendar'; // Calendar 컴포넌트 import
+import React from "react";
 
 /********************************
-    MainPage after success login
+ MainPage after success login
  ********************************/
 function MainPage() {
+
+
     return (
         <div>
-            <h1>Here's Main Page</h1>
-            <h2>let's do this</h2>
+            <h1>Prince Glenn With Evaffanculo</h1>
+            <h2>Porca Eva! Basta!!! Sono stufo delle tue scuse</h2>
+            <h3>Danm! Enough! I'm tired of your excuses </h3>
+
+            {/*  Call : Calendar Component*/}
+            <Calendar />
         </div>
-    );
+);
 }
 export default MainPage
---
+++ src/front/src/pages/Main/calendar.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from "@fullcalendar/interaction";
+
+function Calendar() {
+
+    const [value, onChange] = useState(new Date());
+
+    // dateClick 함수를 선언
+    const dateClick = (info) => {
+        alert(info.dateStr);
+    };
+
+    return (
+        <div>
+            <FullCalendar
+                // 날짜 클릭 시 이벤트 발생
+                dateClick={dateClick}
+                defaultView="dayGridMonth"
+                plugins={[dayGridPlugin, interactionPlugin]}
+                events={[
+                    { title: 'event 1', date: '2023-12-17' },
+                    { title: 'event 2', date: '2023-12-18' }
+                ]}
+            />
+        </div>
+    );
+}
+
+export default Calendar;
