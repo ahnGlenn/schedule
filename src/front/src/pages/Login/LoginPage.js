b34a3b8a069c133988a63cfbdced7a0c8bc22999
ansangja@gmail.com <ansangja@gmail.com>
2024-05-12 22:55:50 +0900

    dev : create LoginPage

--- src/front/src/pages/Login/LoginPage.js
+++ src/front/src/pages/Login/LoginPage.js
@@ -1,6 +1,22 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import "./LoginPage.css";
+import axios from 'axios';
+
 function LoginPage() {
+
+    // 변수 지정
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    async function signUp(){
+        alert("login start");
+
+        try {
+            const data = await axios.get("/api/login", { email, password });
+        } catch {
+            // 오류 발생시 실행
+        }
+    }
 
     return (
         <div className="form signup">
@@ -12,16 +28,13 @@ function LoginPage() {
             <div className="arrow"></div>
             <div className="form-elements">
                 <div className="form-element">
-                    <input type="text" placeholder="Username" />
+                    <input type="text" name="email" placeholder="Email" maxLength="30" onChange={(e) => setEmail(e.target.value)} />
                 </div>
                 <div className="form-element">
-                    <input type="password" placeholder ="Password" />
+                    <input type="password" name="password" placeholder="Password" maxLength="30" onChange={(e) => setPassword(e.target.value)}/>
                 </div>
                 <div className="form-element">
-                    <input type="password" placeholder="Confirm password" />
-                </div>
-                <div className="form-element">
-                    <button id="submit-btn">Sign Up</button>
+                    <button id="submit-btn" onClick={signUp}>Sign Up</button>
                 </div>
             </div>
         </div>
---
+++ src/main/java/com/example/ours/controller/login/LoginController.java
@@ -0,0 +1,46 @@
+package com.example.ours.controller.login;
+
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login(HttpServletResponse response){
+        String results = "";
+        try{
+            System.out.println("test!!!!");
+//            jpaQueryFactory = new JPAQueryFactory(em);
+//            QloginDto qloginDto = QloginDto.loginDto;
+//
+//            loginDto loginUser = jpaQueryFactory
+//                    .select(qloginDto)
+//                    .from(qloginDto)
+//                    .where(qloginDto.email.eq(params.getEmail())
+//                            .and(qloginDto.password.eq(params.getPassword())))
+//                    .fetchOne();
+
+//            if (loginUser != null) {
+//                // 아이디와 패스워드가 일치하는 사용자가 존재하면 로그인 성공
+//                // 여기에서 세션을 설정하거나 토큰을 발급하여 클라이언트에게 전달할 수 있음
+//                // (세션 또는 토큰을 사용하여 로그인 상태를 유지하는 것은 보안적으로 권장됩니다)
+//
+//                results = ">>>>>>>>>>>>>>>>>>>>>>> LOGIN_SUCCESS";
+//            } else {
+//                results = ">>>>>>>>>>>>>>>>>>>>>>> LOGIN_FAILURE";
+//            }
+
+        }catch (Exception e){
+            e.getStackTrace();
+        }
+
+        return results;
+    }
+
+}
+
