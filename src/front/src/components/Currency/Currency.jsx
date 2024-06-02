cfb79d3941efcb6b3a1a66b0e240aabbe46d6ef5
ahnGlenn <dkstkdwo93@naver.com>
2024-06-02 22:46:04 +0900

    dev : Fix Exchange Rate

--- src/front/src/components/Currency/Currency.jsx
+++ src/front/src/components/Currency/Currency.jsx
@@ -8,30 +8,36 @@ function Currency() {
     // 변수 지정
     // -----------------------------
     const [data, setData] = useState([]);
-    const [KOR, setKor] = useState(1);
-    const [EUR, setEur] = useState(1);
+    const [KOR, setKor] = useState([]);
+    const [EUR, setEur] = useState([]);
+    const [calEUR, setCalEur] = useState(1);
+    const [calKOR, setCalKor] = useState(1);
+
 
     // -----------------------------
     // 환율 데이터 가져오기 api
     // -----------------------------
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch('http://localhost:8080/api/currency'); // Java 백엔드의 API URL
-                const result = await response.json();
+    const fetchData = async () => {
+        try {
+            const response = await fetch('http://localhost:8080/api/currency'); // Java 백엔드의 API URL
+            const result = await response.json();
 
-                result.forEach(item =>{
-                    if(item.curUnit == "EUR"){
-                        setEur(parseInt(item.dealBasR.replace(/,/g, '')));
-                    }else{
-                        setKor(parseInt(item.dealBasR.replace(/,/g, '')));
-                    }
-                });
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        };
-        fetchData();
+            result.forEach(item =>{
+                if(item.curUnit == "KRW"){
+                    setEur(parseInt(item.dealBasR.replace(/,/g, '')));
+                    setCalEur(parseInt(item.dealBasR.replace(/,/g, '')));
+                }else{
+                    setKor(parseInt(item.dealBasR.replace(/,/g, '')));
+                    setCalKor(parseInt(item.dealBasR.replace(/,/g, '')));
+                }
+            });
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+    useEffect(() => {
+        fetchData(); // 컴포넌트가 마운트될 때 fetchData 호출
     }, []);
 
     // -----------------------------
@@ -39,40 +45,21 @@ function Currency() {
     // -----------------------------
     const curCal = (e, msg) =>{
         if(msg == "KRW"){
+            alert("kor :" + e.target.value );
             const numVal1 = parseInt(e.target.value);
             const numVal2 = EUR;
-            setKor(numVal1);
-            setEur( numVal1 / numVal2);
+            alert("numVal1 :" + numVal1 + "/numVal2: " + numVal2);
+            setCalKor(numVal1);
+            setCalEur( numVal1 / numVal2);
         }else{
+            alert("eur :" + e.target.value );
             const numVal1 = parseInt(e.target.value);
+            // 여기서 numVal2 초기화 해줘야해
             const numVal2 = KOR;
-            setEur(numVal1);
-            setKor( numVal1 * numVal2);
+            alert("numVal1 :" + numVal1 + "/numVal2: " + numVal2);
+            setCalEur(numVal1);
+            setCalKor( numVal1 * numVal2);
         }
-
-       /* // 기준 환율
-        const dealBasR = EUR;
-
-        // 원화를 유로로 변환하는 함수
-        function krwToEur(krw) {
-            return krw / dealBasR;
-        }
-
-        // 유로를 원화로 변환하는 함수
-        function eurToKrw(eur) {
-            return eur * dealBasR;
-        }
-
-        // 예제: 1원일 때 유로로 변환
-        const krwAmount = 1;
-        const eurAmount = krwToEur(krwAmount);
-        console.log(`${krwAmount} 원은 약 ${eurAmount.toFixed(5)} 유로입니다.`);
-
-// 예제: 1유로일 때 원화로 변환
-        const eurAmountExample = 1;
-        const krwAmountExample = eurToKrw(eurAmountExample);
-        console.log(`${eurAmountExample} 유로는 약 ${krwAmountExample.toFixed(2)} 원입니다.`);*/
-
     }
 
     return (
@@ -86,8 +73,8 @@ function Currency() {
                         <span className="flag _flag krw"></span>
                     </div>
                     <div className="num">
-                        <input onChange={(e) => curCal(e, 'EUR')} id="num" type="text" maxLength="" value={EUR} className="_input"/>
-                        <span className="codeNm">EUR</span>
+                        <input onChange={(e) => curCal(e, 'EUR')} id="num" type="text" maxLength="" value={calEUR} className="_input"/>
+                        <span className="codeNm"> EUR</span>
                     </div>
                 </div>
                 <div className="excr_eq"><span>=</span></div>
@@ -96,8 +83,13 @@ function Currency() {
                         <span className="flag _flag krw"></span>
                     </div>
                     <div className="num">
-                        <input onChange={(e) => curCal(e, 'KRW')} id="num" type="text" maxLength="" value={KOR} className="_input"/>
-                        <span className="codeNm">KRW</span>
+                        <input onChange={(e) => curCal(e, 'KRW')}
+                               id="num"
+                               type="text"
+                               maxLength=""
+                               value={calKOR}
+                               className="_input"/>
+                        <span className="codeNm"> KRW</span>
                     </div>
                 </div>
 
