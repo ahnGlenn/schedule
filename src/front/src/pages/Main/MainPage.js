8ae296683b085aec29e69d8d534b9e42bb33e82e
ahnGlenn <ansangja@gmail.com>
2024-06-10 16:59:06 +0900

    dev : create google map api

--- src/front/src/pages/Main/MainPage.js
+++ src/front/src/pages/Main/MainPage.js
@@ -1,6 +1,7 @@
 import Calendar from './calendar'; // Calendar 컴포넌트 import
 import React from "react";
 import Currency from "../../components/Currency/Currency";
+import Map from "../Main/map";
 
 /********************************
  MainPage after success login
@@ -15,7 +16,10 @@ function MainPage() {
                     <Calendar/>
                 </div>
             </div>
-            <div className="exRateCp" style={{width: '50%'}}>
+            <div className="mapCp" style={{width: '50%'}}>
+                <Map/>
+            </div>
+            <div className="exRateCp" style={{width: '28%'}}>
                 <Currency/>
             </div>
         </div>
