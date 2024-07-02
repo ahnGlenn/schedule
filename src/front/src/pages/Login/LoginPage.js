d8ea7458413948906f3e079d67cc1b05bea10867
ahnGlenn <dkstkdwo93@naver.com>
2024-07-02 16:49:40 +0900

    dev : LoginPage 문구/css 수정

--- src/front/src/pages/Login/LoginPage.js
+++ src/front/src/pages/Login/LoginPage.js
@@ -21,7 +21,7 @@ function LoginPage() {
     /**********************************
      로그인 api
      **********************************/
-    async function signUp(){
+    async function signIn(){
 
         try {
             const loginApi = await axios.get("/api/login", { email, password });
@@ -41,8 +41,8 @@ function LoginPage() {
     return (
         <div className="form signup">
             <div className="form-header">
-                <div className="show-signup">Sign Up</div>
                 <div className="show-signin">Sign In</div>
+                <div className="show-signup">Sign Up</div>
                 <div className="show-reset">Reset</div>
             </div>
             <div className="arrow"></div>
@@ -54,7 +54,7 @@ function LoginPage() {
                     <input type="password" name="password" placeholder="Password" maxLength="30" onChange={(e) => setPassword(e.target.value)}/>
                 </div>
                 <div className="form-element">
-                    <button id="submit-btn" onClick={signUp}>Sign Up</button>
+                    <button id="submit-btn" onClick={signIn}>Sign in</button>
                 </div>
             </div>
         </div>
